f9a0080b78dd45f3823512386da8382673886e74
クルトン <beckeymiku@gmail.com>
2025-07-17 02:49:32 +0900

    最初に開いた時の画面推移

---
+++ src/app/newProject/page.tsx
@@ -0,0 +1,7 @@
+export default function NewProject() {
+  return (
+    <main>
+      <h1>ここに新規プロジェクト</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useProject } from "@/hooks/useProject";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const { project } = useProject();
+  const router = useRouter();
+  useEffect(() => {
+    if (project === null) {
+      router.push("/newProject");
+    }
+  }, [project, router]);
   return (
     <main>
-      <h1>割り勘アプリ</h1>
+      <h1>ここに割り勘画面</h1>
     </main>
   );
 }
